083a0d8790ab07fa479415b29247684e7d9fcd01
ju-voro <voronetskajaj@hotmail.com>
2023-01-11 20:23:05 +0100

    components and debugger

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,59 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function hello() {
+  debugger; //F10, works with browser console only
+  alert('Hello world!');
+}
+  
+
+hello();
+//App component nesting Rating and Accordion components
 function App() {
+  //function operations
+
+  //must return JSX (looks like HTML)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      This is the App component
+      <Rating />
+      <Accordion />
     </div>
   );
 }
 
+
+function Rating() {
+  return(
+    <div>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+    </div>
+    
+  );
+
+}
 export default App;
+
+function Accordion() {
+  return (
+    <div>
+      <h4>Menu</h4>
+      <ul>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+      </ul>
+    </div>
+  );
+}
+
+function Star() {
+  return (
+    <span>★</span>
+  )
+}
